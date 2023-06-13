cd8998266a26be0e12241d5e588cdc9fc3fa6920
Jeff Miles <jeff.miles@computronix.com>
2023-06-13 15:51:50 -0600

    Add Prometheus and Grafana configuration including Windows support

---
+++ terraform/monitoring_win.tf
@@ -0,0 +1,268 @@
+## ---------------------------------------------------
+# Windows exporter configuration
+## ---------------------------------------------------
+resource "kubernetes_manifest" "windows-exporter-daemonset" {
+  manifest = yamldecode(file("windows-exporter-daemonset_daemonset.yaml"))
+}
+
+resource "kubernetes_manifest" "windows-exporter-configmap" {
+  manifest = yamldecode(file("windows-exporter-daemonset_configmap.yaml"))
+}
+
+# Apply the ama-metrics-settings-configmap to your cluster.
+resource "kubernetes_manifest" "ama-metrics-settings-configmap" {
+  manifest = yamldecode(file("ama-metrics-settings-configmap.yaml"))
+}
+
+## ---------------------------------------------------
+# Windows recording rules
+## ---------------------------------------------------
+resource "azurerm_monitor_alert_prometheus_rule_group" "noderecordingrules" {
+  # https://github.com/Azure/prometheus-collector/blob/kaveesh/windows_recording_rules/AddonArmTemplate/WindowsRecordingRuleGroupTemplate/WindowsRecordingRules.json
+  name                = "NodeRecordingRulesRuleGroup-Win-${azurerm_kubernetes_cluster.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  cluster_name        = azurerm_kubernetes_cluster.default.name
+  description         = "Kubernetes Recording Rules RuleGroup for Win"
+  rule_group_enabled  = true
+  interval            = "PT1M"
+  scopes              = [azurerm_monitor_workspace.default.id]
+
+  rule {
+    enabled    = true
+    record     = "node:windows_node:sum"
+    expression = <<EOF
+count (windows_system_system_up_time{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_num_cpu:sum"
+    expression = <<EOF
+count by (instance) (sum by (instance, core) (windows_cpu_time_total{job="windows-exporter"}))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_cpu_utilisation:avg5m"
+    expression = <<EOF
+1 - avg(rate(windows_cpu_time_total{job="windows-exporter",mode="idle"}[5m]))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_cpu_utilisation:avg5m"
+    expression = <<EOF
+1 - avg by (instance) (rate(windows_cpu_time_total{job="windows-exporter",mode="idle"}[5m]))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_memory_utilisation:"
+    expression = <<EOF
+1 -sum(windows_memory_available_bytes{job="windows-exporter"})/sum(windows_os_visible_memory_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_memory_MemFreeCached_bytes:sum"
+    expression = <<EOF
+sum(windows_memory_available_bytes{job="windows-exporter"} + windows_memory_cache_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_totalCached_bytes:sum"
+    expression = <<EOF
+(windows_memory_cache_bytes{job="windows-exporter"} + windows_memory_modified_page_list_bytes{job="windows-exporter"} + windows_memory_standby_cache_core_bytes{job="windows-exporter"} + windows_memory_standby_cache_normal_priority_bytes{job="windows-exporter"} + windows_memory_standby_cache_reserve_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_memory_MemTotal_bytes:sum"
+    expression = <<EOF
+sum(windows_os_visible_memory_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_bytes_available:sum"
+    expression = <<EOF
+sum by (instance) ((windows_memory_available_bytes{job="windows-exporter"}))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_bytes_total:sum"
+    expression = <<EOF
+sum by (instance) (windows_os_visible_memory_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_utilisation:"
+    expression = <<EOF
+(node:windows_node_memory_bytes_total:sum - node:windows_node_memory_bytes_available:sum) / scalar(sum(node:windows_node_memory_bytes_total:sum))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_utilisation:"
+    expression = <<EOF
+1 - (node:windows_node_memory_bytes_available:sum / node:windows_node_memory_bytes_total:sum)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_memory_swap_io_pages:irate"
+    expression = <<EOF
+irate(windows_memory_swap_page_operations_total{job="windows-exporter"}[5m])
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_disk_utilisation:avg_irate"
+    expression = <<EOF
+avg(irate(windows_logical_disk_read_seconds_total{job="windows-exporter"}[5m]) + irate(windows_logical_disk_write_seconds_total{job="windows-exporter"}[5m]))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_disk_utilisation:avg_irate"
+    expression = <<EOF
+avg by (instance) ((irate(windows_logical_disk_read_seconds_total{job="windows-exporter"}[5m]) + irate(windows_logical_disk_write_seconds_total{job="windows-exporter"}[5m])))
+EOF
+  }
+}
+
+resource "azurerm_monitor_alert_prometheus_rule_group" "nodeandkubernetesrules" {
+  # https://github.com/Azure/prometheus-collector/blob/kaveesh/windows_recording_rules/AddonArmTemplate/WindowsRecordingRuleGroupTemplate/WindowsRecordingRules.json
+  name                = "NodeAndKubernetesRecordingRulesRuleGroup-Win-${azurerm_kubernetes_cluster.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  cluster_name        = azurerm_kubernetes_cluster.default.name
+  description         = "Kubernetes Recording Rules RuleGroup for Win"
+  rule_group_enabled  = true
+  interval            = "PT1M"
+  scopes              = [azurerm_monitor_workspace.default.id]
+
+  rule {
+    enabled    = true
+    record     = "node:windows_node_filesystem_usage:"
+    expression = <<EOF
+max by (instance,volume)((windows_logical_disk_size_bytes{job="windows-exporter"} - windows_logical_disk_free_bytes{job="windows-exporter"}) / windows_logical_disk_size_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_filesystem_avail:"
+    expression = <<EOF
+max by (instance, volume) (windows_logical_disk_free_bytes{job="windows-exporter"} / windows_logical_disk_size_bytes{job="windows-exporter"})
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_net_utilisation:sum_irate"
+    expression = <<EOF
+sum(irate(windows_net_bytes_total{job="windows-exporter"}[5m]))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_net_utilisation:sum_irate"
+    expression = <<EOF
+sum by (instance) ((irate(windows_net_bytes_total{job="windows-exporter"}[5m])))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = ":windows_node_net_saturation:sum_irate"
+    expression = <<EOF
+sum(irate(windows_net_packets_received_discarded_total{job="windows-exporter"}[5m])) + sum(irate(windows_net_packets_outbound_discarded_total{job="windows-exporter"}[5m]))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "node:windows_node_net_saturation:sum_irate"
+    expression = <<EOF
+sum by (instance) ((irate(windows_net_packets_received_discarded_total{job="windows-exporter"}[5m]) + irate(windows_net_packets_outbound_discarded_total{job="windows-exporter"}[5m])))
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_pod_container_available"
+    expression = <<EOF
+windows_container_available{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_container_total_runtime"
+    expression = <<EOF
+windows_container_cpu_usage_seconds_total{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_container_memory_usage"
+    expression = <<EOF
+windows_container_memory_usage_commit_bytes{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_container_private_working_set_usage"
+    expression = <<EOF
+windows_container_memory_usage_private_working_set_bytes{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_container_network_received_bytes_total"
+    expression = <<EOF
+windows_container_network_receive_bytes_total{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "windows_container_network_transmitted_bytes_total"
+    expression = <<EOF
+windows_container_network_transmit_bytes_total{job="windows-exporter"} * on(container_id) group_left(container, pod, namespace) max(kube_pod_container_info{job="kube-state-metrics"}) by(container, container_id, pod, namespace)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "kube_pod_windows_container_resource_memory_request"
+    expression = <<EOF
+max by (namespace, pod, container) (kube_pod_container_resource_requests{resource="memory",job="kube-state-metrics"}) * on(container,pod,namespace) (windows_pod_container_available)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "kube_pod_windows_container_resource_memory_limit"
+    expression = <<EOF
+kube_pod_container_resource_limits{resource="memory",job="kube-state-metrics"} * on(container,pod,namespace) (windows_pod_container_available)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "kube_pod_windows_container_resource_cpu_cores_request"
+    expression = <<EOF
+max by (namespace, pod, container) ( kube_pod_container_resource_requests{resource="cpu",job="kube-state-metrics"}) * on(container,pod,namespace) (windows_pod_container_available)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "kube_pod_windows_container_resource_cpu_cores_limit"
+    expression = <<EOF
+kube_pod_container_resource_limits{resource="cpu",job="kube-state-metrics"} * on(container,pod,namespace) (windows_pod_container_available)
+EOF
+  }
+  rule {
+    enabled    = true
+    record     = "namespace_pod_container:windows_container_cpu_usage_seconds_total:sum_rate"
+    expression = <<EOF
+sum by (namespace, pod, container) (rate(windows_container_total_runtime{}[5m]))
+EOF
+  }
+}
