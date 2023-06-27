09c4551343b279134bfe4bbaa22773e6c0e06d84
Jeff Miles <jeff.miles@computronix.com>
2023-06-26 21:01:07 -0600

    Add kubectl provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,9 @@ terraform {
     kubernetes = {
       source = "registry.terraform.io/hashicorp/kubernetes"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+    }
     helm = {
       source = "registry.terraform.io/hashicorp/helm"
     }
@@ -23,6 +26,14 @@ provider "azurerm" {
   environment = "public"
 }
 provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.default.kube_admin_config.0.host
+  username               = azurerm_kubernetes_cluster.default.kube_admin_config.0.username
+  password               = azurerm_kubernetes_cluster.default.kube_admin_config.0.password
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_admin_config.0.cluster_ca_certificate)
+}
+provider "kubectl" {
   host                   = azurerm_kubernetes_cluster.default.kube_admin_config.0.host
   username               = azurerm_kubernetes_cluster.default.kube_admin_config.0.username
   password               = azurerm_kubernetes_cluster.default.kube_admin_config.0.password
